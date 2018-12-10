3c5571306b7b7dcaf3a6bf292e109f384351daea
rubinoAM <parkcathedralgrace@gmail.com>
2018-12-10 10:24:38 -0500

    TKinter Updates V0.1

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ import tkinter as tk
 
 # TKinter
 root = tk.Tk()
-ball = tk.PhotoImage(file="eightball.gif")
+root.title('Magic Eight Ball')
 
-w1 = tk.Label(root, image=ball).pack(side="right")
-w2 = tk.Label(root,justify=tk.LEFT,text="Lay It Down! Lay It Down!").pack(side="left")
+win_x = int(root.winfo_screenwidth()/2 - root.winfo_reqwidth()/2)
+win_y = int(root.winfo_screenheight()/2 - root.winfo_reqheight()/2)
+
+root.geometry("550x400+{}+{}".format(win_x, win_y))
+
+start_text = "Ah, yes. The old magic eight ball!\nLet's dust this sucker off and give a quick shake, shall we?"
+start_msg = tk.Message(root, text=start_text)
+start_msg.config(bg='chartreuse',font=('Helvetica',24,"bold"))
+start_msg.pack()
+
+#ball = tk.PhotoImage(file="eightball.gif")
+#w = tk.Label(root, image=ball).pack()
 
 root.mainloop()
 
