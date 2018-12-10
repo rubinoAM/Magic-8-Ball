6c6b1bb44c76da6aed66d7845835103a1961965e
rubinoAM <parkcathedralgrace@gmail.com>
2018-12-10 09:51:42 -0500

    Import and test TKinter

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+# Imports
 from eightball import *
-mjg = Eightball()
-print(mjg.roll())
+import tkinter as tk
+
+# TKinter
+root = tk.Tk()
+ball = tk.PhotoImage(file="eightball.gif")
+
+w1 = tk.Label(root, image=ball).pack(side="right")
+w2 = tk.Label(root,justify=tk.LEFT,text="Lay It Down! Lay It Down!").pack(side="left")
+
+root.mainloop()
+
+"""mjg = Eightball()
+print(mjg.roll())"""
